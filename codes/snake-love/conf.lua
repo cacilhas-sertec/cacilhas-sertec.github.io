df7414c297afaa3493d98d3df2487f71a0155f14
Montegasppα Cacilhας <montegasppa@cacilhas.info>
2023-09-25 17:18:18 -0300

    add resources for LÖVE

---
+++ codes/snake-love/conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+  t.version = "11.5"
+  t.window.title = "LÖVE Snake"
+  t.window.width = 960
+  t.window.height = 960
+  t.window.resizable = false
+  t.modules.physics = false
+end
